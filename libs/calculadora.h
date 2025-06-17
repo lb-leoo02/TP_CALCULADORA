d71663c29d06c481be7135d63a4645ab41e691ee
lb-leoo02 <bylion14lb@gmail.com>
2025-06-16 21:39:54 -0300

    Update calculadora.h

--- libs/calculadora.h
+++ libs/calculadora.h
@@ -6,20 +6,25 @@
 #include "Vector.h"
 #include "arbol_math.h"
 
+
 #define TAM_ECU 512
 
 typedef struct{
-    nodo *arbol_ecu;    //ecuacion
-    char ecu[TAM_ECU];
+    nodo *arbol_ecu;    //arbol de ecuacion
+    char ecu[TAM_ECU];  //char de ecuacion
 }ecuacion;
 
+void limpiarBuffer();
 char menu();
 void quitarespacios(char* ecu);
-int ingresarecu(ecuacion*, size_t*);
-void mostrarecuaciones(ecuacion* );
+int ingresarecu(ecuacion*, size_t*,int,void*);
+void mostrarecuaciones(ecuacion*, size_t);
 //int AniadirEcuacion(ecuacion*, size_t*);
 void ayuda();
+void borrarecuacion(ecuacion*, size_t*);
 void destruirecuaciones(ecuacion*, size_t);
+
+void evaluarecuacion(nodo*arbol, double valor); 
 
 
 
