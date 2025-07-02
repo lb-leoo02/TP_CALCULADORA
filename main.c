bfd95486177a0860dfec8c4e1cca047182e0d12f
lb-leoo02 <bylion14lb@gmail.com>
2025-07-01 21:08:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,25 +2,28 @@
 #include <stdlib.h>
 #include "libs/calculadora.h"
 #include "libs/Archivos.h"
+#include "libs/calculadora_bmp.h"
 int main()
 {
     ecuacion ecuaciones[10]; 
     size_t cecu=0;
     char op, subop;
-    int band=0, salir=1;
-    contarArchivos(&band);
+    int arch_guardados=0, salir=1;
+    contarArchivos(&arch_guardados);
+    int indice;
 
     while(salir){
         op=menu();
         switch(op){
             case 'A':
                 if(cecu!=0){
+                    limpiarPantalla();
                     subop = pedir_opcion("ABX",
-                    "Ingresar Ecuación:\n"
-                    "[A] - Agregar ecuación nueva.\n"
-                    "[B] - Cambiar ecuación existente.\n"
+                    "--- INGRESAR ECUACION ---\n"
+                    "[A] - Agregar ecuacion nueva.\n"
+                    "[B] - Cambiar ecuacion existente.\n"
                     "[X] - Volver.\n"
-                    "Ingrese una opción: "
+                    "Ingrese una opcion: "
                     );
 
                     if(subop=='B'){
@@ -35,55 +38,71 @@ int main()
                     puts("No se pudo aniadir ecuacion");
                 break;
             case 'B':
-                mostrarecuaciones(ecuaciones, cecu);
+                mostrarecuaciones(ecuaciones, cecu,1);
+                EnterParaSalir();
                 break;
             case 'C':
-                if(band<3){
-                    if(!GuardaArchivos(ecuaciones, cecu, &band)){
+                if(arch_guardados<10){
+                    if(!GuardaArchivos(ecuaciones, cecu, &arch_guardados)){
                         borrarecuacion(ecuaciones,&cecu);
                     }
                 }
                 else
                     puts("No podes guardar mas ecuaciones. Utilice la opcion [E] para borrar las ecuaciones guardadas");
+
+                EnterParaSalir();
                 break;
             case 'D':
                 CargarArchivo(ecuaciones,&cecu);
                 break;
             case 'E':
                 borrarArchivos();
-                band=0;
+                arch_guardados=0;
+                EnterParaSalir();
                 break;
             case 'F':
-                subop = pedir_opcion("ABFX",
-                    "Evaluar Ecuación:\n"
-                    "[A] - Ingresar una tabla de valores.\n"
-                    "[B] - Ingresar un solo valor para una incógnita.\n"
-                    "[C] - Buscar raices.\n"
-                    "[X] - Volver.\n"
-                    "Ingrese una opción: "
-                );
-                if(subop=='A'){
-                    EvaluarXeY(ecuaciones, cecu);
+                if(cecu != 0){
+                    limpiarPantalla();
+                    printf("--- EVALUAR ECUACION ---\n\n");
+                    subop = pedir_opcion("ABCX",
+                        "[A] - Ingresar una tabla de valores.\n"
+                        "[B] - Ingresar un solo valor para una incognita.\n"
+                        "[C] - Buscar raices.\n"
+                        "[X] - Volver.\n"
+                        "Ingrese una opcion: "
+                    );
+                    if(subop=='A'){
+                        EvaluarXeY(ecuaciones, cecu);
+                    }
+                    else if(subop=='B'){
+                        TablaAlrededorDeValor(ecuaciones,cecu);
+                        EnterParaSalir();
+                    }
+                    else if(subop=='C'){
+                        BuscarRaices(ecuaciones, cecu);
+                    }
                 }
-                else if(subop=='B'){
-                    GenerarTabla(ecuaciones,cecu);
-                }
-                else if(subop=='F'){
-                    //hacer algo
+                else {
+                    puts("No hay ecuaciones cargadas para evaluar.");
                 }
                 break;
             case 'G':
-                puts("opcion G");
+                if(cecu == 0) {
+                    puts("No hay ecuaciones cargadas para graficar.");
+                    break;
+                }
+                graficar(ecuaciones,cecu,&indice);
                 break;
             case 'H':
-                ayuda();
+                menu_ayuda_interactivo();
+                EnterParaSalir();
                 break;
             case 'X':
                 subop = pedir_opcion("SN",
                     "Seguro quieres salir? S/N: "
                 );
                 if(subop=='S'){
-                    printf("\nSaliendo del programa...");
+                    printf("\nSaliendo del programa...\n");
                     salir=0;
                 }
                 break;
